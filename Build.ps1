547c7f2934923401ae2dcce062a9231f4cbeca2e
“venkatesan <vselvam@vmware.com>
2019-02-09 18:20:21 +0530

    build_cmds

--- Build.ps1
+++ Build.ps1
@@ -1,2 +1,3 @@
 .\Nuget.exe restore chess.sln
-msbuild chess.sln
+$path = (Get-Item -Path ".\").FullName + "\output"
+msbuild chess.sln /p:Configuration=Release /p:DeployOnBuild=True /p:DeployDefaultTarget=WebPublish /p:WebPublishMethod=FileSystem /p:DeleteExistingFiles=True /p:publishUrl=$path
